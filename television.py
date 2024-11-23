341d12584ed27e37fe1c289fa6d7b83e911c2282
Jordy <jordybeni867@gmail.com>
2024-11-22 20:10:46 -0600

    Removed main.py

---
+++ television.py
@@ -0,0 +1,55 @@
+class Television:
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
+
+    def __init__(self) -> None:
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = self.MIN_VOLUME
+        self.__channel: int = self.MIN_CHANNEL
+
+    def power(self):
+        if self.__status:
+            self.__status = False
+        else:
+            self.__status = True
+
+    def mute(self):
+        if self.__muted:
+            self.__muted = False
+        else:
+            self.__muted = True
+            self.__volume = self.MIN_VOLUME
+
+    def channel_up(self):
+        if self.__status and self.__channel < self.MAX_CHANNEL:
+            self.__channel += 1
+        elif self.__status and self.__channel == self.MAX_CHANNEL:
+            self.__channel = self.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.__status and self.__channel > self.MIN_CHANNEL:
+            self.__channel -= 1
+        elif self.__status and self.__channel == self.MIN_CHANNEL:
+            self.__channel = self.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.__status and self.__volume < self.MAX_VOLUME:
+            if self.__muted:
+                self.mute()
+            self.__volume += 1
+        elif self.__status and self.__volume == self.MAX_VOLUME:
+            return
+
+    def volume_down(self):
+        if self.__status and self.__volume > self.MIN_VOLUME:
+            if self.__muted:
+                self.mute()
+            self.__volume -= 1
+        elif self.__status and self.__volume == self.MIN_VOLUME:
+            return
+
+    def __str__(self) -> str:
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
